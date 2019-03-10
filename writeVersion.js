81d77b58b27b6dd2b8051ab48d7d5eebad3d6689
fp51 <abhineet.mishra@tesco.com>
2019-03-10 08:07:30 +0000

    Adding updated packageJson versioning

--- writeVersion.js
+++ writeVersion.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const coPackage = require('./package.json');
-const innerPackage = require('./locales/package.json');
 const sem = require('semver');
 const path = require('path');
 const execSync = require('child_process').execSync;
@@ -14,7 +13,7 @@ const run = (command, options) => execSync(command, {
 // const getRootPath = () => run('git rev-parse --show-cdup').trim();
 // const getPackageJsonPath = path.join(process.cwd(), `${ getRootPath() }package.json`);
 // const getPackageLockJsonPath = path.join(process.cwd(), `${ getRootPath() }package-lock.json`, );
-console.log('This is the current version --> ', innerPackage.version);
+console.log('This is the current version --> ', coPackage.version);
 const standard_input = process.stdin;
 standard_input.setEncoding('utf-8');
 console.log('To which release version would you like to update?');
